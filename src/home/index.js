cf3435126b031d2bdc3584a0d2d2c4d432751f75
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-06 22:06:50 -0700

    Created index.js and imported makeVessel function, added event listener

---
+++ src/home/index.js
@@ -0,0 +1,11 @@
+import makeVessel from './make-vessel.js';
+
+const vesselForm = document.getElementById('vessel-form');
+
+vesselForm.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const formData = new FormData(vesselForm);
+    const vessel = makeVessel(formData);
+    console.log('vessel', vessel);
+});
