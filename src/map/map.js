7f108ae83930800a99e3e7828df8ab27899bd4b8
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-08 17:42:16 -0700

    Removed code and stored in function makeProfile

--- src/map/map.js
+++ src/map/map.js
@@ -1,16 +1,4 @@
 import api from '../services/api.js';
+import makeProfile from '../make-profile.js';
 
-const name = document.getElementById('name');
-const vesselLogo = document.getElementById('vessel-logo');
-const hp = document.getElementById('hp');
-const gas = document.getElementById('gas');
-const confidence = document.getElementById('confidence');
-
-
-const vessel = api.getVessel();
-
-name.textContent = '"' + vessel.name + '"';
-vesselLogo.src = '/assets/' + vessel.type + '.svg';
-hp.textContent = 'HP: ' + vessel.hp;
-gas.textContent = 'GAS: ' + vessel.gas;
-confidence.textContent = 'CONFIDENCE: ' + vessel.confidence;
+makeProfile();
