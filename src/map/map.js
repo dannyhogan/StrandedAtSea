506ae8de1680eaddd13bbb65299e3f672c5ecb35
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-08 21:28:59 -0700

    imported createQuestLink and createCompletedQuest

--- src/map/map.js
+++ src/map/map.js
@@ -1,4 +1,7 @@
 import api from '../services/api.js';
 import makeProfile from '../make-profile.js';
+import createQuestLink from './create-quest-links.js';
+import createCompletedQuest from './create-completed-quest.js';
 
+console.log(makeProfile());
 makeProfile();
