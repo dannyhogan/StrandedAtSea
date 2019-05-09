609e360b82ff083220666368130d8e502b2dfc39
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-08 17:40:50 -0700

    Created a makeProfile function and stored it in its own file

---
+++ src/make-profile.js
@@ -0,0 +1,19 @@
+import api from './services/api.js';
+
+function makeProfile() {
+    const name = document.getElementById('name');
+    const vesselLogo = document.getElementById('vessel-logo');
+    const hp = document.getElementById('hp');
+    const gas = document.getElementById('gas');
+    const confidence = document.getElementById('confidence');
+
+    const vessel = api.getVessel();
+
+    name.textContent = '"' + vessel.name + '"';
+    vesselLogo.src = '/assets/' + vessel.type + '.svg';
+    hp.textContent = 'HP: ' + vessel.hp;
+    gas.textContent = 'GAS: ' + vessel.gas;
+    confidence.textContent = 'CONFIDENCE: ' + vessel.confidence;
+}
+
+export default makeProfile;
