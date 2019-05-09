2d0fbcabc773738a25c7b79171f67c19f2066141
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-08 23:54:34 -0700

    replaced each quest link with an image

--- src/map/create-quest-links.js
+++ src/map/create-quest-links.js
@@ -1,14 +1,18 @@
 function createQuestLink(quest) {
     const link = document.createElement('a');
+    const img = document.createElement('img');
     link.classList.add('quest');
 
     const searchParams = new URLSearchParams();
     searchParams.set('id', quest.id);
     link.href = 'quest.html?' + searchParams.toString();
 
-    link.textContent = quest.title;
     link.style.top = quest.map.top;
     link.style.right = quest.map.right;
+
+    img.src = './assets/' + quest.id + '.png';
+    link.append(img);
+
     return link;
 }
 
