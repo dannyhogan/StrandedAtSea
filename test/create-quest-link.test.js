5faf950041a8674e2297cc6a67d03a63b926c41a
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-08 18:37:16 -0700

    Separated createQuestLink function from test

--- test/create-quest-link.test.js
+++ test/create-quest-link.test.js
@@ -1,3 +1,4 @@
+import createQuestLink from '../src/map/create-quest-links.js';
 const test = QUnit.test;
 
 test('takes a quest object and returns HTML for a quest link that you can append to the list', assert => {
@@ -13,15 +14,3 @@ test('takes a quest object and returns HTML for a quest link that you can append
     //assert
     assert.equal(result.outerHTML, expected);
 });
-
-function createQuestLink(quest) {
-    const link = document.createElement('a');
-    link.classList.add('quest');
-
-    const searchParams = new URLSearchParams();
-    searchParams.set('id', quest.id);
-    link.href = 'quest.html?' + searchParams.toString();
-
-    link.textContent = quest.title;
-    return link;
-}
