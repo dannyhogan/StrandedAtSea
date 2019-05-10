16f9518fb785e8960ebb08b34ec906b9c7e7c446
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-10 11:28:06 -0700

    Imported findByd and scoreQuest functions, added to event listener

--- src/quest/quest.js
+++ src/quest/quest.js
@@ -1,16 +1,21 @@
 import api from '../services/api.js';
 import createChoice from './createChoice.js';
 import makeProfile from '../make-profile.js';
-
-const questTitle = document.getElementById('quest-title');
-const questDescription = document.getElementById('quest-description');
-const choicesParent = document.getElementById('choices-parent');
-const questForm = document.getElementById('quest-form');
+import findById from '../find-by-id.js';
+import scoreQuest from './score-quest.js';
 
 // Grabs the current quest by ID from searchParams
 const searchParams = new URLSearchParams(window.location.search);
 const questId = searchParams.get('id');
 const quest = api.getQuestById(questId);
+
+makeProfile();
+
+// Grabs needed DOM elements on quest page
+const questTitle = document.getElementById('quest-title');
+const questDescription = document.getElementById('quest-description');
+const choicesParent = document.getElementById('choices-parent');
+const questForm = document.getElementById('quest-form');
 
 questTitle.textContent = quest.title;
 questDescription.textContent = quest.description;
@@ -21,15 +26,15 @@ for(let i = 0; i < quest.choices.length; i++) {
     choicesParent.appendChild(choiceHTML);
 }
 
-makeProfile();
-
 questForm.addEventListener('submit', event => {
     event.preventDefault();
 
     const formData = new FormData(questForm);
-    const choice = formData.get('choice');
+    const choiceId = formData.get('choice');
 
-    const selectedChoice = quest.choices[choice];
-    console.log(selectedChoice);
+    const selectedChoice = findById(quest.choices, choiceId);
+    const vessel = api.getVessel();
+    const score = scoreQuest(vessel, selectedChoice);
+    console.log(score);
 
 });
