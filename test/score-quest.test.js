4bbd12ca2fec7fd431a1996091632956fb31296d
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-10 10:07:40 -0700

    Created working tests for each scoreQuest stat that is updated by user choice

---
+++ test/score-quest.test.js
@@ -0,0 +1,92 @@
+const test = QUnit.test;
+
+QUnit.module('score quest tests for each stat');
+
+test('determines gas based off choice', assert => {
+    //act
+    const vessel = {
+        gas: 100,
+        hp: 100,
+        confidence: 0
+    };
+
+    const choice = {
+        gas: -50,
+        hp: 0,
+        confidence: 0
+    };
+
+    const expected = {
+        gas: 50,
+        hp: 100,
+        confidence: 0
+    };
+
+    //arrange
+    const result = scoreQuest(vessel, choice);
+
+    //assert
+    assert.deepEqual(result, expected);
+});
+
+test('determines hp based off choice', assert => {
+    //act
+    const vessel = {
+        hp: 10,
+        gas: 20,
+        confidence: 0
+    };
+
+    const choice = {
+        hp: -5,
+        gas: 10,
+        confidence: 0
+    };
+
+    const expected = {
+        hp: 5,
+        gas: 30,
+        confidence: 0
+    };
+
+    //arrange
+    const result = scoreQuest(vessel, choice);
+
+    //assert
+    assert.deepEqual(result, expected)
+});
+
+test('determines confidence based off choice', assert => {
+    //act
+    const vessel = {
+        confidence: 0,
+        gas: 0,
+        hp: 10
+    };
+
+    const choice = {
+        confidence: 10,
+        gas: 0,
+        hp: 10
+    };
+
+    const expected = {
+        confidence: 10,
+        gas: 0,
+        hp: 20
+    };
+
+    //arrange
+    const result = scoreQuest(vessel, choice);
+
+    //assert
+    assert.deepEqual(result, expected)
+});
+
+function scoreQuest(vessel, choice) {
+    vessel.gas += choice.gas;
+    vessel.hp += choice.hp;
+    vessel.confidence += choice.confidence;
+
+    return vessel;
+}
--- test/tests.js
+++ test/tests.js
@@ -3,3 +3,4 @@ import './api.test.js';
 import './create-quest-link.test.js';
 import './create-completed-quest.test.js';
 import './createChoice.test.js';
+import './score-quest.test.js';
