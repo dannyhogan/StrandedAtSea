180a259820f6e036d94c81a5a95230ba7e78d5a2
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-09 23:50:34 -0700

    Imported needed dom elements and modules to update quest.html

--- src/quest/quest.js
+++ src/quest/quest.js
@@ -1,3 +1,20 @@
+import api from '../services/api.js';
+import createChoice from './createChoice.js';
 import makeProfile from '../make-profile.js';
+
+const questTitle = document.getElementById('quest-title');
+const choicesParent = document.getElementById('choices-parent');
+
+const searchParams = new URLSearchParams(window.location.search);
+const questId = searchParams.get('id');
+const quest = api.getQuestById(questId);
+
+questTitle.textContent = quest.title;
+
+for(let i = 0; i < quest.choices.length; i++) {
+    const choice = quest.choices[i];
+    const choiceHTML = createChoice(choice);
+    choicesParent.appendChild(choiceHTML);
+}
 
 makeProfile();
