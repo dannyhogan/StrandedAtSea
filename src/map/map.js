e04b644fcab6a04d403822d8f06f372c0c377eb0
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-12 23:57:47 -0700

    Addec check to see if vessel is dead or all quests are completed to map.js

--- src/map/map.js
+++ src/map/map.js
@@ -1,10 +1,18 @@
 import api from '../services/api.js';
 import makeProfile from '../make-profile.js';
 import createQuestLink from './create-quest-links.js';
+import isDead from './is-dead.js';
+import hasCompletedAllQuests from './has-completed-all-quests.js';
+
 // import createCompletedQuest from './create-completed-quest.js';
 
 const quests = api.getQuests();
 const vessel = api.getVessel();
+
+if(isDead(vessel) || hasCompletedAllQuests(vessel, quests)) {
+    window.location = 'end.html';
+}
+
 const boatImage = document.getElementById('boat');
 boatImage.src = '/assets/boats/' + vessel.type + '.png';
 
